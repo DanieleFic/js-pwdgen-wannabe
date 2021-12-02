1fc6def7b8e7e69c8e63e0af39c6b042ee41ccda
DanieleFic <reb_ficus22@hotmail.it>
2021-12-02 14:56:40 +0100

    first commit

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+let userInputName = prompt ("Inserici Nome")
+
+let userInputSurName = prompt ("Inserici Cognome")
+
+let userInputFavColor = prompt ("Inserici Colore Preferito")
+
+const userNumber = 21
+
+document.getElementById('userInfo').innerHTML = userInputName +  userInputSurName +  userInputFavColor + userNumber
